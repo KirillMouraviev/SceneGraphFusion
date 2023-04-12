ab07517cabf6e42ededd3f9436ccc78aca7da7ac
kirillMouraviev <kirill.mouraviev@yandex.ru>
2023-04-12 16:37:50 +0300

    Add object-by-room clustering

--- scene_graph_parser.py
+++ scene_graph_parser.py
@@ -33,6 +33,38 @@ for node_type in all_types:
     json.dump(graph_of_type, fout)
     fout.close()
 
+def find_source(node_id, node_type):
+    nodes = []
+    for edge in j['edges']:
+        if edge['target'] == node_id:
+            source_node = node_by_id[edge['source']]
+            if source_node['attributes']['type'].startswith(node_type):
+                nodes.append(source_node)
+    #for node in nodes:
+    #    print(node['attributes']['type'][:-14])
+    return nodes
+
+object_nodes = [node for node in j['nodes'] if node['attributes']['type'].startswith('Object')]
+object_clusters = {}
+for i, node in enumerate(object_nodes):
+    place_nodes = find_source(node['id'], 'Place')
+    room_nodes = []
+    for place_node in place_nodes:
+        room_nodes = room_nodes + find_source(place_node['id'], 'Room')
+    if len(room_nodes) > 0:
+        parent_node_id = room_nodes[0]['id']
+    elif len(place_nodes) > 0:
+        parent_node_id = place_nodes[0]['id']
+    else:
+        parent_node_id = node['id']
+    if parent_node_id not in object_clusters:
+        object_clusters[parent_node_id] = [node]
+    else:
+        object_clusters[parent_node_id].append(node)
+
+print('Number of objects:', len(object_nodes))
+print('Number of object clusters:', len(object_clusters))
+
 fout = open('graph_dumps/full_graph.json', 'w')
 json.dump(j, fout)
 fout.close()
